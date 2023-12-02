6c1d840ecfcb72a4081d0a0b704565c2b51dc3b1
Elif Genc <elif.genc@avkaryazilim.com.tr>
2023-12-02 14:30:18 +0300

    Idle ve running animasyonları oluşturuldu. Player hareketine uygun animasyonlar kullanıldı. Kamera kontrolü düzenlendi.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rigidbody2D.velocity = new Vector2(7f * dirX, rigidbody2D.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rigidbody2D.velocity = new Vector2(dirX * moveSpeed, rigidbody2D.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, 14f);
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, jumpForce);
+        }
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0)
+        {
+            animator.SetBool("isRunning", true);
+            spriteRenderer.flipX = false;
+        }
+        else if (dirX < 0)
+        {
+            animator.SetBool("isRunning", true);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
         }
     }
 }
