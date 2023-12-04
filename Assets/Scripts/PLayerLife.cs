a3ffda7b38b52e99ed8a1c1f24dd457ed7148713
Elif Genc <elif.genc@avkaryazilim.com.tr>
2023-12-04 14:00:25 +0300

    Oyuncunun ortadan kaldırılması için animasyon oluşturuldu. Oyuncu spikes'e çarptığında aktif sahnenin tekrar başlatılması sağlandı.

---
+++ Assets/Scripts/PLayerLife.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PLayerLife : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rigidbody2D.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
