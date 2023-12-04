b10686122bac29815549ce04dd786b2d16b6670e
Elif Genc <elif.genc@avkaryazilim.com.tr>
2023-12-04 13:23:07 +0300

    Kiraz için animasyon oluşturuldu. Player'ın topladığı elemanlar için ortak bir yapı kuruldu. Toplanan kiraz sayısı ekrana yazıldı.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+    private BoxCollider2D boxCollider2D;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private enum MovementState { idle, running, jumping, falling }
 
@@ -19,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -26,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rigidbody2D.velocity = new Vector2(dirX * moveSpeed, rigidbody2D.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, jumpForce);
         }
@@ -61,5 +65,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         animator.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
